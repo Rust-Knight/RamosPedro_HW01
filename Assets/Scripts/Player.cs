b0f1cd6b1273c025af528b2bb3ea204149479d94
Peter <pedroramoshernandez01@gmail.com>
2022-08-30 00:07:57 -0500

    Added 2D audio code
    Add audio source script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(TankController))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _maxHealth = 3;
+    int _currentHelath;
+
+    TankController _tankController;
+    private void awake()
     {
-        
+        _tankController = GetComponent<TankController>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void start ()
     {
-        
+        _currentHelath = _maxHealth;
     }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHelath += amount; 
+        
+        _currentHelath = Mathf.Clamp(_currentHelath, 0, _maxHealth);
+        Debug.Log("Player's health: " + _currentHelath);
+
+    }
+    
+    public void DecreaseHealth(int amount)
+    {
+        _currentHelath -= amount;
+        Debug.Log("Player's health: " + _currentHelath);
+        if(_currentHelath <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        // play particles 
+        // play sound
+    }
+
 }
